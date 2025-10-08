c823fb66da22b803121217d67c52ecb582d726da
Jhes Fogaça <jessicafogaca60@gmail.com>
2025-10-07 23:59:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+let cards = document.querySelectorAll('.card')
+
+function rotateCards() {
+    let angle = 0;
+    cards.forEach((card, index) => {
+        if (card.classList.contains('away')) {
+            card.style.transform = `translateY(-120vh) rotate(-48deg)`;
+        } else {
+            card.style.transform = `rotate(${angle}deg)`;
+            angle = angle - 10;
+            card.style.zIndez = cards.length - index;
+        }
+    });
+}
+
+let stackArea = document.querySelector('.stack-area');
+
+window.addEventListener('scroll', () => {
+    let distance = window.innerHeight / 2;
+    let topVal = stackArea.getBoundingClientRect().top
+    let index = -1 * (topVal / distance + 1);
+    index = Math.floor(index);
+    for (i = 0; i < cards.length; i++) {
+        if (i <= index) {
+            cards[i].classList.add('away');
+        } else {
+            cards[i].classList.remove('away');
+        }
+    }
+    rotateCards()
+});
+
